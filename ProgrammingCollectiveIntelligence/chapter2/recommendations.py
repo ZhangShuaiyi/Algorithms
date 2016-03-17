bcc12ec8e717e0a6abec3edb13146f59582c9218
ZhangShy <zaozh1990@163.com>
2016-03-17 19:27:30 +0800

    皮尔逊相关系数计算相似度

--- ProgrammingCollectiveIntelligence/chapter2/recommendations.py
+++ ProgrammingCollectiveIntelligence/chapter2/recommendations.py
@@ -30,8 +30,8 @@ critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
            }
 
 
-# 返回一个有关person1和person2的基于距离的相似度评价
-def sim_distance(prefs, person1, person2):
+# 返回一个有关person1和person2的基于欧氏距离的相似度评价
+def sim_euclidean_distance(prefs, person1, person2):
     si = [item for item in prefs[person1] if item in prefs[person2]]
     # 如果没有相同之处，则返回0
     if len(si) == 0:
@@ -56,6 +56,39 @@ def sim_distance2(prefs, person1, person2):
     return 1 / (1 + sqrt(sum_of_squares))
 
 
+# 皮尔森相关系数
+# 结果的取值区间为[-1，1]，-1表示完全的负相关(这个变量下降，那个就会上升)，+1表示完全的正相关，0表示没有线性相关
+def sim_pearson_correlation_score(prefs, p1, p2):
+    si = [item for item in prefs[p1] if item in prefs[p2]]
+    n = len(si)
+    
+    # 如果没有共同之处返回0
+    if n == 0:
+        return 0
+
+    # 对所有偏好求和
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+
+    # 求平方和
+    sum1Sq = sum([pow(prefs[p1][it], 2) for it in si])
+    sum2Sq = sum([pow(prefs[p2][it], 2) for it in si])
+
+    # 求乘积之和
+    pSum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+
+    # 计算皮尔逊相关系数
+    num = pSum - (sum1 * sum2 / n)
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+    if den == 0:
+        return 0
+
+    r = num / den
+    return r
+
+
 if __name__ == '__main__':
-    ret = sim_distance2(critics, 'Lisa Rose', 'Gene Seymour')
-    print(ret)
+    ret = sim_euclidean_distance(critics, 'Lisa Rose', 'Gene Seymour')
+    print("Euclidean Distance: %.20f" % (ret))
+    ret = sim_pearson_correlation_score(critics, 'Lisa Rose', 'Gene Seymour')
+    print("Pearson Correlation Score: %.20f" % (ret))
