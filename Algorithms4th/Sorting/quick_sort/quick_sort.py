582bd3afc87e3002d84cafefeeae98dce3b3e479
ZhangShy <zaozh1990@163.com>
2016-03-26 16:57:22 +0800

    快速排序

---
+++ Algorithms4th/Sorting/quick_sort/quick_sort.py
@@ -0,0 +1,38 @@
+class QuickSort(object):
+    """快速排序"""
+
+    @classmethod
+    def sort(cls, a):
+        cls.sort_lh(a, 0, len(a) - 1)
+
+    @classmethod
+    def partition(cls, a, lo, hi):
+        # 左右扫描索引
+        i = lo + 1
+        j = hi
+        # 切分元素
+        v = a[lo]
+        while True:
+            # 扫描左右，检查扫描是否结束并交换元素
+            while a[i] < v:
+                if i == hi:
+                    break
+                i += 1
+            while v < a[j]:
+                if j == lo:
+                    break
+                j -= 1
+            if i >= j:
+                break
+            a[i], a[j] = a[j], a[i]
+        a[lo], a[j] = a[j], a[lo]
+        # 结果a[lo..j-1] <= a[j] <= a[j+1..hi]
+        return j
+
+    @classmethod
+    def sort_lh(cls, a, lo, hi):
+        if hi <= lo:
+            return
+        j = cls.partition(a, lo, hi)
+        cls.sort_lh(a, lo, j - 1)
+        cls.sort_lh(a, j + 1, hi)
