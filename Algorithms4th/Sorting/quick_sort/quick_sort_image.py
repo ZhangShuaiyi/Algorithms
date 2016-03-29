7337365ff2dcbaed4c984b100838acf3ed4025c4
ZhangShy <zaozh1990@163.com>
2016-03-29 19:22:48 +0800

    修改使用PIL保存快速排序的步骤

--- Algorithms4th/Sorting/quick_sort/quick_sort_image.py
+++ Algorithms4th/Sorting/quick_sort/quick_sort_image.py
@@ -22,28 +22,40 @@ class QuickSortImage(QuickSort):
         while True:
             # 扫描左右，检查扫描是否结束并交换元素
             while a[i] < v:
+                cls.draw_list(a, lo, hi, i, j)
                 if i == hi:
                     break
                 i += 1
             while v < a[j]:
+                cls.draw_list(a, lo, hi, i, j)
                 if j == lo:
                     break
                 j -= 1
             if i >= j:
                 break
-            cls.draw_list(a, lo, hi, i, j)
+            cls.draw_list(a, lo, hi, i, j, swap=True)
             a[i], a[j] = a[j], a[i]
-        cls.draw_list(a, lo, hi, lo, j)
+        cls.draw_list(a, lo, hi, lo, j, swap=True)
         a[lo], a[j] = a[j], a[lo]
         # 结果a[lo..j-1] <= a[j] <= a[j+1..hi]
         return j
 
     @classmethod
-    def draw_list(cls, a, lo=-1, hi=-1, i=-1, j=-1):
+    def draw_list(cls, a, lo=-1, hi=-1, i=-1, j=-1, swap=False):
         # 新建一个白色背景图片
         img = Image.new('RGB', (cls.w, cls.h), (255, 255, 255))
         draw = ImageDraw.Draw(img)
         x = 10
+        swap_y = 20
+        cmp_y = 10
+        if i != -1 and j != -1:
+            draw.line((x + cls.xs * lo, cmp_y, x + cls.xs * i, cmp_y),
+                      fill=(0, 0, 255))
+            draw.line((x + cls.xs * j, cmp_y, x + cls.xs * hi, cmp_y),
+                      fill=(0, 0, 255))
+            if swap:
+                draw.line((cls.xs * i + x, swap_y, cls.xs * j + x, swap_y),
+                          fill=(255, 0, 0), width=2)
         for n, d in enumerate(a):
             s = ''
             if isinstance(d, str):
@@ -63,9 +75,14 @@ class QuickSortImage(QuickSort):
                 draw.line((x, cls.h, x, cls.h - d * cls.ys),
                           fill=(192, 192, 192), width=cls.lw)
             if n == i or n == j:
-                draw.line((x, cls.h - d * cls.ys + 12, x, cls.h - d * cls.ys),
-                          fill=(255, 0, 0), width=cls.lw)
-                draw.text((x, cls.h - d * cls.ys - 12), s, (255, 0, 0))
+                # draw.line((x, cls.h - d * cls.ys + 12, x, cls.h -
+                #            d * cls.ys), fill=(255, 0, 0), width=cls.lw)
+                # draw.text((x, cls.h - d * cls.ys - 12), s, (255, 0, 0))
+                draw.line((x, cls.h - d * cls.ys - 12, x, cmp_y),
+                          fill=(0, 0, 255))
+                if swap:
+                    draw.line((x, cls.h - d * cls.ys - 12, x, swap_y),
+                              fill=(255, 0, 0), width=2)
             x += cls.xs
         # 显示图片
         # img.show()
@@ -81,7 +98,7 @@ class QuickSortImage(QuickSort):
         if isinstance(max_a, str):
             max_a = int(max_a, 36)
         # y坐标的缩放值
-        cls.ys = (cls.h - 20.0) / max_a
+        cls.ys = (cls.h - 40.0) / max_a
         # x坐标的缩放值
         cls.xs = (cls.w - 10.0) / len(a)
         cls.lw = int(cls.xs) - 1
